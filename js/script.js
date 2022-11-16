cf76fab8478f5f8cbda306d7ded03185f7401723
Justyna Boesche <justyna.boesche@gmail.com>
2022-11-16 17:29:47 +0100

    formatting changes in js made

--- js/script.js
+++ js/script.js
@@ -27,7 +27,6 @@
         const amountElement = document.querySelector(".js-amount");
         const currencyElement = document.querySelector(".js-currency");
  
-
         const amount = +amountElement.value;
         const currency = currencyElement.value;
 
